0bde5fe872bbfa8e6251477747466e707c042fae
Ezi4zy <Ezi4zy@163.com>
2020-12-09 10:09:50 +0800

    merge two sorted lists

---
+++ 21.合并两个有序链表.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode.cn id=21 lang=python
+#
+# [21] 合并两个有序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+class Solution1(object):
+    
+    def merge(self, node, l1, l2):
+        if l1 and l2:
+            if l1.val < l2.val:
+                node.next = l1
+                l1 = l1.next
+            else:
+                node.next = l2
+                l2 = l2.next
+            self.merge(node.next, l1, l2)
+        elif l1 and not l2:
+            node.next = l1
+        elif not l1 and l2:
+            node.next = l2
+        else:
+            return
+    
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode()
+        self.merge(head, l1, l2)
+        return head.next
+
+# @lc code=end
+

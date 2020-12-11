2060d09355b83633de1fe67781c9846eb084e747
Ezi4zy <Ezi4zy@163.com>
2020-12-11 10:31:18 +0800

    implement strstr

---
+++ 28.实现-str-str.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=28 lang=python
+#
+# [28] 实现 strStr()
+#
+
+# @lc code=start
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        else:
+            return -1
+
+            
+            
+# @lc code=end
+

a3b416e024fe0856afb9f973087b40e94d33020d
Ezi4zy <Ezi4zy@163.com>
2020-12-15 16:23:25 +0800

    remove duplicates from sorted list

---
+++ 83.删除排序链表中的重复元素.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=83 lang=python
+#
+# [83] 删除排序链表中的重复元素
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        node = head
+        while node:
+            next_ = node.next
+            while next_ and next_.val == node.val:
+                next_ = next_.next
+            node.next = next_
+            node = next_
+        return head            
+# @lc code=end
+

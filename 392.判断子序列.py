76c3f01a9f3a38304fdd53c3b8c2707ee0f65117
Ezi4zy <Ezi4zy@163.com>
2020-12-30 19:18:22 +0800

    is subsequence

---
+++ 392.判断子序列.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=392 lang=python
+#
+# [392] 判断子序列
+#
+
+# @lc code=start
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        index_s = 0
+        index_t = 0
+        while index_s < len(s) and index_t < len(t):
+            if s[index_s] == t[index_t]:
+                index_s += 1
+            index_t += 1
+        return index_s == len(s)
+# @lc code=end
+

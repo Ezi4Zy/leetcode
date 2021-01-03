b19c9b95e710445ae42aeda75cb9541db8d70180
Ezi4zy <Ezi4zy@163.com>
2021-01-03 19:26:03 +0800

    fibonacci number

---
+++ 509.斐波那契数.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=509 lang=python
+#
+# [509] 斐波那契数
+#
+
+# @lc code=start
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 2:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+# @lc code=end
+

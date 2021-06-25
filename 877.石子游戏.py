d1b63710cf4c1bf9c7c222e173f76e1b53a96aa3
Ezi4zy <Ezi4zy@163.com>
2021-06-25 18:02:38 +0800

    stone game

---
+++ 877.石子游戏.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=877 lang=python
+#
+# [877] 石子游戏
+#
+
+# @lc code=start
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        return True
+# @lc code=end
+
